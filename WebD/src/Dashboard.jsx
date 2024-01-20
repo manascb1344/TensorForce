84b6de6656c3ad69ab24533b04c686d5f45fc253
kyash99252 <yashkadam99252@gmail.com>
2024-01-20 17:45:09 +0530

    Folder restructured

--- WebD/src/Dashboard.jsx
+++ WebD/src/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from "axios";
+import { Navbar } from "./components";
+import styles from "./style";
 
 const Dashboard = () => {
   const { isAuthenticated, user } = useAuth0();
@@ -39,9 +41,12 @@ const Dashboard = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <h2>{user.name}</h2>
-        <h2>{user.email}</h2>
+      <div className="bg-primary w-full overflow-hidden">
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
       </div>
     )
   );
