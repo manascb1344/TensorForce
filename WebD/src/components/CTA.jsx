7d9bc1dbeb0ece82431b6d79be993fdfe1bd2d79
kyash99252 <yashkadam99252@gmail.com>
2024-02-08 23:41:00 +0530

    All photos added + Spelling Mistakes

--- WebD/src/components/CTA.jsx
+++ WebD/src/components/CTA.jsx
@@ -1,24 +1,40 @@
 import styles from "../style";
 import Button from "./Button";
-import { robot } from "../assets";
+import Lottie from "react-lottie";
+import animationData from "../assets/business-team.json";
 
-const CTA = () => (
-	<section
-		className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
-	>
-		<div className="flex-1 flex flex-col">
-			<h2 className={styles.heading2}>
-				Start Predicting <br className="sm:block hidden" /> with TensorForce
-			</h2>
-			<p className={`${styles.paragraph} max-w-[470px] mt-5`}>Optimize your Investments Now</p>
-			<div className="mt-10">
-				<Button label="Sign Up Now" />
-			</div>
-		</div>
-		<div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-			<img src={robot} alt="billing" className="w-[500px] h-[500px] relative z-[5]" />
-		</div>
-	</section>
-);
+const CTA = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <section
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
+      <div className="flex-1 flex flex-col">
+        <h2 className={styles.heading2}>
+          Start Predicting <br className="sm:block hidden" /> with TensorForce
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Optimize your Investments Now
+        </p>
+        <div className="mt-10">
+          <Button label="Sign Up Now" />
+        </div>
+      </div>
+      <div
+        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
+      >
+        <Lottie options={defaultOptions} height={500} width={500} />
+      </div>
+    </section>
+  );
+};
 
 export default CTA;
--- WebD/src/components/CardDeal.jsx
+++ WebD/src/components/CardDeal.jsx
@@ -1,25 +1,40 @@
-import { card } from "../assets";
 import styles, { layout } from "../style";
 import Button from "./Button";
+import Lottie from "react-lottie";
+import animationData from "../assets/Cb7PYb6sBk.json";
 
-const CardDeal = () => (
-	<section className={layout.section}>
-		<div className={layout.sectionInfo}>
-			<h2 className={styles.heading2}>
-				Track your Performance <br className="sm:block hidden" />& Improve your Stratergies.
-			</h2>
-			<p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-				With our performance tracker, you can easily monitor and analyze your stock prediction models' performance. Gain valuable
-				insights and make data-driven decisions to improve your strategies and maximize your returns.
-			</p>
+const CardDeal = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-			<Button label="Track Performance" styles="mt-10" />
-		</div>
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Track your Performance <br className="sm:block hidden" />& Improve
+          your Strategies.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With our performance tracker, you can easily monitor and analyze your
+          stock prediction models' performance. Gain valuable insights and make
+          data-driven decisions to improve your strategies and maximize your
+          returns.
+        </p>
 
-		<div className={layout.sectionImg}>
-			<img src={card} alt="card" className="w-[100%] h-[100%]" />
-		</div>
-	</section>
-);
+        <Button label="Track Performance" styles="mt-10" />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <Lottie options={defaultOptions} height="100%" width="100%" />
+      </div>
+    </section>
+  );
+};
 
 export default CardDeal;
